dbe0562f4d82449e0e39e1b836cca18b699636d1
masaun <masanoriuno@gmail.com>
2019-09-22 09:40:27 +0200

    Fix import path of openzeppelin-solidity modules in 'chainlink/contracts/Oracle.sol'

--- contracts/lib/Oracle.sol
+++ contracts/lib/Oracle.sol
@@ -1,4 +1,3 @@
 pragma solidity 0.4.24;
 
-import "chainlink/node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
-//import "chainlink/contracts/Oracle.sol";
+import "chainlink/contracts/Oracle.sol";
