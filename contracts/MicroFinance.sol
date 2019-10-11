a6e0d29dea1b3313676c53dbb5ac1245bb09094a
masaun <masanoriuno@gmail.com>
2019-10-11 11:38:39 +0200

    Add directly of modifier and storage

--- contracts/MicroFinance.sol
+++ contracts/MicroFinance.sol
@@ -3,8 +3,11 @@ pragma solidity 0.4.24;
 import "chainlink/node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "chainlink/node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
+import "./storage/MfStorage.sol";
+import "./modifiers/MfOwnable.sol";
 
-contract MicroFinance is Ownable {
+
+contract MicroFinance is Ownable, MfStorage, MfOwnable {
 
     using SafeMath for uint256;
 
